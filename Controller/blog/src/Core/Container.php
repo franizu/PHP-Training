f5c012e683dcd419e90ff476f3a1efa344001786
Robert S <robert.schmidtgen@web.de>
2019-04-02 09:57:31 +0200

    Aenderung vom 02.04.19

--- Controller/blog/src/Core/Container.php
+++ Controller/blog/src/Core/Container.php
@@ -1,5 +1,12 @@
 <?php
 
+/* Dateiname: Container.php
+*  Autor: RS
+*  Training PHP Udemy - Blog-Programmierung
+*  Funktion: Erzeugung von Objekten für Datenbankabruf
+*
+*/
+
 namespace App\Core;
 
 use PDO;
@@ -8,23 +15,34 @@ use App\Post\PostsController;
 
 class Container
 {
-
   private $receipts = [];
   private $instances = [];
 
   public function __construct()
   {
+    /*
+      * Funktion: Erzeugt eine Bauanleitung für die einzelnen Objekte
+      * Parameter:keine
+      * Ergebnis: Array welches Closures für Datenbankabruf enthält
+
+  // Bauanleitung Erzeugung eines PostsControllers
+      */
     $this->receipts = [
       'postsController' => function() {
         return new PostsController(
           $this->make('postsRepository')
         );
       },
+
+      // Bauanleitung zur Erzeugung eines PostsRepositories
+
       'postsRepository' => function() {
         return new PostsRepository(
           $this->make("pdo")
         );
       },
+
+      //Bauanleitung zur Erzeugung eines Objektes zum Aufbau einer Datenbankverbindung
       'pdo' => function() {
         $pdo = new PDO(
           'mysql:host=localhost;dbname=blog;charset=utf8',
@@ -39,7 +57,12 @@ class Container
 
   public function make($name)
   {
-    if (!empty($this->instances[$name]))
+      /*
+      * Funktion: make-Funktion erzeugt die Objekte
+      * Parameter: $name - postsController, postsRepository, pdo
+      * Ergebnis: Objekte für Datenbankabruf
+      */
+    if (!empty($this->instances[$name])) // Verhindert, dass mehrere Datenbankverbindungen aufgebaut werden
     {
       return $this->instances[$name];
     }
@@ -50,35 +73,6 @@ class Container
 
     return $this->instances[$name];
   }
-  /*
-  private $pdo;
-  private $postsRepository;
-
-  public function getPdo()
-  {
-    if (!empty($this->pdo)) {
-      return $this->pdo;
-    }
-    $this->pdo = new PDO(
-      'mysql:host=localhost;dbname=blog;charset=utf8',
-      'blog',
-      'TX4LQBEzfZfVqnLV'
-    );
-    $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    return $this->pdo;
-  }
-
-  public function getPostsRepository()
-  {
-    if (!empty($this->postsRepository)) {
-      return $this->postsRepository;
-    }
-    $this->postsRepository = new PostsRepository(
-      $this->getPdo()
-    );
-    return $this->postsRepository;
-  }
-  */
 
 }
  ?>
